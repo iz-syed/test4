e5effef8a6c5b13d4758cc9c3513586e2fdb73be
Mubarak Cc7 <iz-syed@users.noreply.github.com>
2021-11-07 14:22:07 +0530

    Route + Login + Dashboard Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlankComponent } from './layouts/blank/blank.component';
+import { FullComponent } from './layouts/full/full.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:BlankComponent,
+    children:[
+      {
+        path:'',
+        loadChildren:()=>import('./modules/auth/auth.module').then(m=>m.AuthModule)
+      }
+    ],
+  },
+  {
+    path:'dashboard',
+    component:FullComponent,
+    children:[
+      {
+        path:'',
+        loadChildren:()=>import('./modules/dashboard/dashboard.module').then(m=>m.DashboardModule)
+      }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
